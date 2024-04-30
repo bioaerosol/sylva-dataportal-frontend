da82fd85f4827d2e34be529a6bd419fd6b870f43
Robert Gebauer <robert.gebauer@gmx.de>
2024-04-30 22:26:51 +0200

    Bugfix text color.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import NavigationBar from '@/components/NavigationBar.vue';
+import NavigationBar from '@/components/NavigationBar.vue'
 </script>
 
 <template>
@@ -17,28 +17,20 @@ import NavigationBar from '@/components/NavigationBar.vue';
   </div>
 
   <div class="container border-top mt-5 ps-5 pe-5 bg-light">
-  <footer class="py-5">
-    <div class="row">
-      <div class="col-12">
-        <p>
-          The overall goal of SYLVA is to achieve a radical improvement and fill gaps in temporal resolution, timeliness, coverage, and availability of information about aeroallergens and other 
-          bioaerosols. SYLVA technological innovations will be accompanied with new infrastructure, distribution and exploitation pathways, and links with stakeholders to ensure technology uptake
-          and sustainability beyond the lifetime of the project. &mdash; <a href="https://sylva.bioaerosol.eu/">https://sylva.bioaerosol.eu/</a>
-        </p>
+    <footer class="py-5">
+      <div class="row">
+        <div class="col-12 text-dark">
+          <p>The overall goal of SYLVA is to achieve a radical improvement and fill gaps in temporal resolution, timeliness, coverage, and availability of information about aeroallergens and other bioaerosols. SYLVA technological innovations will be accompanied with new infrastructure, distribution and exploitation pathways, and links with stakeholders to ensure technology uptake and sustainability beyond the lifetime of the project. &mdash; <a href="https://sylva.bioaerosol.eu/">https://sylva.bioaerosol.eu/</a></p>
+        </div>
       </div>
-    </div>
 
-    <div class=" py-4 my-4 d-flex justify-content-start">
-      <img src="@/assets/EU.svg" alt="Funded by the European Union" />
-      <img src="@/assets/SERI.svg" alt="Finish Meteorological Institute" />
-    </div>
-        
-  </footer>
-</div>
-
-
+      <div class="py-4 my-4 d-flex justify-content-between">
+        <img src="@/assets/EU.svg" alt="Funded by the European Union" />
+        <img src="@/assets/SERI.svg" alt="Finish Meteorological Institute" />
+      </div>
+    </footer>
+  </div>
 </template>
-
 
 <script>
 export default {

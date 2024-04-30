6e98aec2e9bce68cb213d62dc783058fbd7539e4
Robert Gebauer <robert.gebauer@gmx.de>
2024-04-30 21:31:48 +0200

    Added footer.

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,30 @@ import NavigationBar from '@/components/NavigationBar.vue';
       </div>
     </div>
   </div>
+
+  <div class="container border-top mt-5 ps-5 pe-5 bg-light">
+  <footer class="py-5">
+    <div class="row">
+      <div class="col-12">
+        <p>
+          The overall goal of SYLVA is to achieve a radical improvement and fill gaps in temporal resolution, timeliness, coverage, and availability of information about aeroallergens and other 
+          bioaerosols. SYLVA technological innovations will be accompanied with new infrastructure, distribution and exploitation pathways, and links with stakeholders to ensure technology uptake
+          and sustainability beyond the lifetime of the project. &mdash; <a href="https://sylva.bioaerosol.eu/">https://sylva.bioaerosol.eu/</a>
+        </p>
+      </div>
+    </div>
+
+    <div class=" py-4 my-4 d-flex justify-content-start">
+      <img src="@/assets/EU.svg" alt="Funded by the European Union" />
+      <img src="@/assets/SERI.svg" alt="Finish Meteorological Institute" />
+    </div>
+        
+  </footer>
+</div>
+
+
 </template>
+
 
 <script>
 export default {
@@ -24,3 +47,16 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+footer {
+  img {
+    height: 10rem;
+    max-width: 30vw;
+
+    &:first-child {
+      margin-right: 3rem;
+    }
+  }
+}
+</style>

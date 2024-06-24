8388514f37bb8c861a1ebd79969a392da6423d87
Robert Gebauer <robert.gebauer@gmx.de>
2024-06-24 21:08:59 +0200

    Added Poleno reference dataset link.

--- src/views/ReferenceDatasetsView.vue
+++ src/views/ReferenceDatasetsView.vue
@@ -4,12 +4,26 @@
   <h1>Reference Datasets</h1>
   <p class="sectionIntro">SYLVA real-time monitoring devices use Machine Learning (ML) algorithms, which may require further development in efficiency. To do this, ML has to be trained and evaluated using real previously categorized data by human experts (labelled data). In this section, we supply you with data that is organized as having one reference dataset per device type.</p>
 
-  <div class="card mt-5" style="width: 18rem">
-    <font-awesome-icon :icon="['fas', 'file-zipper']" class="card-img-top"/>
-    <div class="card-body">
-      <h5 class="card-title">BAA500</h5>
-      <p class="card-text">Reference datasets of device type BAA500 in version 2024-05-23.</p>
-      <a href="/workspace/referencedatasets/reference_dataset_pomo_20240523.zip" class="btn btn-primary w-100">Download (~ 1GiB)</a>
+  <div class="row">
+    <div class="col-12 col-md-6 col-lg-3">
+      <div class="card mt-5 w-100">
+        <font-awesome-icon :icon="['fas', 'file-zipper']" class="card-img-top" />
+        <div class="card-body">
+          <h5 class="card-title">BAA500</h5>
+          <p class="card-text">Reference datasets of device type BAA500 in version 2024-05-23.</p>
+          <a href="/workspace/referencedatasets/reference_dataset_pomo_20240523.zip" class="btn btn-primary w-100">download (~ 1GiB)</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-12 col-md-6 col-lg-3">
+      <div class="card mt-5 w-100">
+        <font-awesome-icon :icon="['fas', 'file-zipper']" class="card-img-top" />
+        <div class="card-body">
+          <h5 class="card-title">Poleno</h5>
+          <p class="card-text">Reference datasets of device type Poleno in version 2024-06-23.</p>
+          <a href="/workspace/referencedatasets/poleno" class="btn btn-primary w-100">view file list</a>
+        </div>
+      </div>
     </div>
   </div>
 </template>

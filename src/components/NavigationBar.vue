46d3043fc16d778314008dc9cda04fdae17d29c5
Robert Gebauer <robert.gebauer@gmx.de>
2024-09-15 19:11:40 +0200

    Fixed navbar link to SYLVA project site

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -11,7 +11,7 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item me-3">
-                  <a href="https://sylva.bioaerosol.eu/"><font-awesome-icon :icon="['fas', 'house']" class="me-2" />SYLVA Home</a>
+                  <a href="https://sylva.bioaerosol.eu/" class="nav-link"><font-awesome-icon :icon="['fas', 'house']" class="me-2" />SYLVA Project Site</a>
               </li>
               <li class="nav-item me-3">
                   <router-link to="/dashboard" class="nav-link"><font-awesome-icon :icon="['fas', 'gauge-high']" class="me-2" />Dashboard</router-link>

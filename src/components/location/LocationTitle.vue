da49854c7a6197a8e4072ed620c9e6816e8ef548
salashhab <salashhab@ua.es>
2025-03-11 11:31:39 +0100

    add Sweden flag

--- src/components/location/LocationTitle.vue
+++ src/components/location/LocationTitle.vue
@@ -28,6 +28,7 @@ const flagUrls = {
   DE: new URL('@/assets/flags/DE.png', import.meta.url).href,
   ES: new URL('@/assets/flags/ES.png', import.meta.url).href,
   FI: new URL('@/assets/flags/FI.png', import.meta.url).href,
-  CH: new URL('@/assets/flags/CH.png', import.meta.url).href
+  CH: new URL('@/assets/flags/CH.png', import.meta.url).href,
+  SE: new URL('@/assets/flags/SE.png', import.meta.url).href
 }
 </script>
